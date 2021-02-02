759d19a6b6e8cd4c19384702996a28fb5c97c9eb
devansh2008dg <74973237+devansh2008dg@users.noreply.github.com>
2021-02-02 19:04:16 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,22 @@
+class Paper{
+    constructor(x,y,r){
+        var options = {
+        'isStatic':false,
+        'restitution':0.3,
+        'friction':0.5,
+        'densiy':2
+        }
+        this.body = Bodies.circle(x,y,r,options);
+        World.add(world, this.body);  
+    }
+    display(){
+        var pos=this.body.position;
+        push();
+        translate(pos.x,pos.y);
+        ellipseMode(RADIUS);
+        ellipse(0,0,20,20);
+        pop();
+
+    }
+}
+ 
